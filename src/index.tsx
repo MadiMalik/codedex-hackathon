c93b58ad918f6398280c115622325d5fb627a20e
debasishrowlo <debasishrowlo@gmail.com>
2023-12-13 02:51:05 +0530

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import { createRoot } from "react-dom/client"
+
+const App = () => {
+  return (
+    <div>
+      code
+    </div>
+  )
+}
+
+createRoot(document.getElementById("app")).render(<App />)
